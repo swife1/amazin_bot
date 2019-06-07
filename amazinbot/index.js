a9dbc1780ce3a1971098fb07523d957d39d188dd
swife1 <ntpedersen@alaska.edu>
2019-06-06 22:31:09 -0800

    Update

--- amazinbot/index.js
+++ amazinbot/index.js
@@ -8,7 +8,21 @@ client.on('message', message => {
 
     if (!message.author.bot && (msg.includes('amazing') || msg.includes('amazin')))
     {
-        message.channel.send("https://streamable.com/rexhb")
+        let d = new Date(message.createdTimestamp);
+        message.channel.send("https://streamable.com/rexhb");
+        console.log(d + "\n" + message.author.username + ' - ' + msg + '\n');
+    }
+    if (!message.author.bot && (msg.includes('beta') || msg.includes('beyta')))
+    {
+        let d = new Date(message.createdTimestamp);
+        message.channel.send("https://streamable.com/w9isd");
+        console.log(d + "\n" + message.author.username + ' - ' + msg + '\n');
+    }
+    else if (!message.author.bot && (msg.includes('tormund') && msg.includes('cry')))
+    {
+        let d = new Date(message.createdTimestamp);
+        message.channel.send("https://i.imgur.com/fUtQxbw.jpg")
+        console.log(d + "\n" + message.author.username + ' - ' + msg + '\n');
     }
 
 })
